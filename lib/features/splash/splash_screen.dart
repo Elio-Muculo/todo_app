1d701b38393f38a5c32f315890ca0cb3a0f25155
Elliot <emuculo25@gmail.com>
2022-08-20 22:12:18 +0200

    add call for other page after 3s

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
-
+import 'package:todo_app/core/app_images.dart';
+import 'package:todo_app/features/main_page/main_page.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context) => const MainPage()
+      ));
+    });
+
     return Scaffold(
       body: Center(
-        child: Image.asset(AppImages.todo)
+        child: Image.asset(AppImages.todoLogo)
       )
     );
   }
