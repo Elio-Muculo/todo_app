46e549c6542cd93b564b8e7733e0591a24d6e692
Elliot <emuculo25@gmail.com>
2022-08-25 23:11:41 +0200

    fix value notifier class

--- lib/features/main_page/controller/homepagecontroller.dart
+++ lib/features/main_page/controller/homepagecontroller.dart
@@ -1,29 +1,29 @@
 
 import 'package:flutter/material.dart';
 
-// class HomeController extends ValueNotifier<int> {
-//   HomeController({int? value}) : super(value ?? 0);
-//
-//   void increment() {
-//     value++;
-//   }
-//
-// }
-
-
-class HomeController extends ChangeNotifier {
-  int _counter = 0;
-
-  int get counter => _counter;
+class HomeController extends ValueNotifier<int> {
+  HomeController({int? value}) : super(value ?? 0);
 
   void increment() {
-    _counter++;
-    notifyListeners();
-  }
-
-  void decrement() {
-    _counter--;
-    notifyListeners();
+    value++;
   }
 
 }
+
+
+// class HomeController extends ChangeNotifier {
+//   int _counter = 0;
+//
+//   int get counter => _counter;
+//
+//   void increment() {
+//     _counter++;
+//     notifyListeners();
+//   }
+//
+//   void decrement() {
+//     _counter--;
+//     notifyListeners();
+//   }
+//
+// }
